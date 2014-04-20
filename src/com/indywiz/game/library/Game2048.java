7fb809d1bc4d5e6ad72b8b804dc97a8b3a254bd9
Viswanathan R <vichu.wizard@gmail.com>
2014-04-19 22:02:57 -0500

    Added few tests and basic initialization of grid in Game2048.java

--- src/com/indywiz/game/library/Game2048.java
+++ src/com/indywiz/game/library/Game2048.java
@@ -4,4 +4,23 @@ package com.indywiz.game.library;
  * Created by viswanathanr on 4/11/14.
  */
 public class Game2048 {
+
+    private final int GRID_SIZE = 4;
+    public int[][] gridArray;
+
+    public Game2048() {
+        gridArray = new int[GRID_SIZE][GRID_SIZE];
+    }
+
+
+    public boolean initializeBoard(int row, int column) {
+        if(gridArray == null ||
+                (row >= GRID_SIZE) || (column >= GRID_SIZE) ||
+                (row < 0) || (column < 0) ) {
+            return false;
+        }
+
+        gridArray[row][column] = 2;
+        return true;
+    }
 }
--- test/Game2048Test.java
+++ test/Game2048Test.java
@@ -20,6 +20,44 @@ public class Game2048Test extends TestCase {
         assertTrue(true);
     }
 
+    @Test
+    public void testInitializeBoardWithAllEmptyStates() {
+        assertNotNull(game2048.gridArray);
+    }
+
+    @Test
+    public void testBoardSetUpInSquare() {
+        for (int i=0; i<4; i++)
+            assertEquals(4, game2048.gridArray[i].length);
+    }
+
+    @Test
+    public void testSetUpBoardWithInitalTwo() {
+        game2048.initializeBoard(2, 2);
+        assertEquals(2, game2048.gridArray[2][2]);
+    }
+
+    @Test
+    public void testSetUpBoardWithGridArrayBeingNull() {
+        game2048.gridArray = null;
+        assertFalse(game2048.initializeBoard(2, 2));
+    }
+
+    @Test
+    public void testSetUpBoardWithGridArrayIndexBeingInvalid() {
+        assertFalse(game2048.initializeBoard(5, 5));
+    }
+
+    @Test
+    public void testSetUpBoardWithRowBeingInvalid() {
+        assertFalse(game2048.initializeBoard(5, 2));
+    }
+
+    @Test
+    public void testSetUpBoardWithRowAndColValueBeingNegative() {
+        assertFalse(game2048.initializeBoard(-1, 2));
+    }
+
 
 
 }
